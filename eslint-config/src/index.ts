a92f37a12a86eff87220cd76789973ec6215a3bd
C. Lewis <ctj.lewis@icloud.com>
2022-06-21 10:31:11 -0500

    chore: initial commit

---
+++ eslint-config/src/index.ts
@@ -0,0 +1,9 @@
+export const eslintConfig = {
+  "extends": [
+    "plugin:@typescript-eslint/recommended"
+  ],
+  "parser": "@typescript-eslint/parser",
+  "plugins": ["@typescript-eslint"],
+};
+
+export default eslintConfig;
